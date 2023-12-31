4b41f7430fd08aa57939a7e85eba69127ee5b942
Maria Matveeva <marikadenisova@yandex.ru>
2023-12-31 03:07:06 +0300

    express mesto second

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
-const app = require('../app');
 
 router.use('/users', userRouter);
 
 router.use('/cards', cardRouter);
 
-app.use((req, res) => {
+router.use((req, res) => {
   res.status(404).send({ message: 'Страница не найдена' });
 });
 
