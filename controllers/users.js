cf72c3acb6552ff38c0f2341f0a2e4e46ee9dd8c
Maria Matveeva <marikadenisova@yandex.ru>
2024-01-20 18:35:21 +0300

    mesto express with authorization first

--- controllers/users.js
+++ controllers/users.js
@@ -50,7 +50,7 @@ module.exports.createUser = (req, res, next) => {
     }))
     .catch((error) => {
       if (error.code === AUTH_ERROR) {
-        next(new ConflictError('Пользователь с данным email уже существует'));
+        res.status(409).send({ message: 'Пользователь с данным email уже существует' });
       } else if (error.name === 'ValidationError') {
         next(new BadRequestError('Переданы некорректные данные'));
       } else {
