11cdfe89367786d1d144e45b4c2e58719a896a3e
Maria Matveeva <marikadenisova@yandex.ru>
2024-01-21 15:19:57 +0300

    mesto express with authorization first

--- controllers/users.js
+++ controllers/users.js
@@ -31,7 +31,7 @@ module.exports.getUserById = (req, res, next) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (!user) {
-        next(new NotFoundError('Пользователь по указанному id не найден.'));
+        res.status(404).send({ message: 'Пользователь по указанному id не найден.' });
         return;
       }
       res.status(200).send(user);
